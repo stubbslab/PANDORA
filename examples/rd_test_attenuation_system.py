918d10c29fa9f9463910d8ca0f2d02bcb9e6078a
Johnny Esteves <johnny.udi@gmail.com>
2025-02-04 09:45:01 -0800

    debug v10

--- examples/rd_test_attenuation_system.py
+++ examples/rd_test_attenuation_system.py
@@ -1,26 +1,30 @@
 import sys
+import numpy as np
 sys.path.append("/Users/pandora_ctrl/Documents/dev/versions/v1.0/PANDORA")
 
 from pandora.pandora_controller import PandoraBox
-from pandora.utils.random import head
+from pandora.utils.random import head, line
 
 head("Test: Characterization of the Attenuation System")
 print("Description: This test will loop over zaber ND filters and monochromator wavelengths.")
-print("----------------------------------------------------------")
+line()
 
 # Start measurement cycle
 # run_id = int(input("Enter the run ID: "))
 expTime = 1.0 # sec
 expTimeDark = 0.25 # sec
-print("----------------------------------------------------------")
+line()
 
 # Create a PandoraBox object
 pandora_box = PandoraBox("../default.yaml")
 # pandora_box.start_run(run_id) # makes a catalog with name run_id:05d
+import logging
+pandora_box.logger.addHandler(logging.NullHandler())
 
 # Initialize the Pandora System Components
 pandora_box.initialize_subsystems()
 
+out = '../data/test/{:04d}_{}_{}'
 for wav in [400, 500, 600, 700]:
     head(f"Starting wavelength: {wav} nm")
     # Move monochromator to 400 nm
@@ -30,16 +34,19 @@ for wav in [400, 500, 600, 700]:
     ods = ["CLEAR", "OD20", "OD15", "OD10", "OD05"]
     for od in ods:
         pandora_box.zaber.z1.move_to_slot(od)
-        pandora_box.take_exposure(expTime)
+        d1, d2 = pandora_box.take_exposure(expTime)
+        np.save(out.format(wav, od, 'd1'),d1)
+        np.save(out.format(wav, od, 'd2'),d2)
+
         # pandora_box.take_dark(expTimeDark)
         # pandora_box.take_measurement(expTime)
         # pandora_box.take_dark(expTimeDark)
     print(f"Finsihed wavelength {wav} nm")
-    print("----------------------------------------------------------")
+    line()
 
 pandora_box.close_all_connections()
 head("Optical measurement cycle completed.")
 
 head("Wavelength cycle completed.")
-print(f"Measurements saved on {pandora_box.run_info['output']}")
+# print(f"Measurements saved on {pandora_box.run_info['output']}")
 print("Goodbye!")
--- examples/test.py
+++ examples/test.py
@@ -1,4 +1,31 @@
 import sys
 sys.path.append("/Users/pandora_ctrl/Documents/dev/versions/v1.0/PANDORA")
-from pandora.pandora_controller import PandoraBox
-pandora_box = PandoraBox("../default.yaml")
+# from pandora.pandora_controller import PandoraBox
+# pandora_box = PandoraBox("../default.yaml")
+
+from pandora.commands.keysight import KeysightState
+settings = {
+            'mode': "CURR",
+            'rang': 'AUTO',
+            'nplc': 1.,
+            'nsamples': 10,
+            'delay': 0.,
+            'interval': 2e-3,
+            }
+
+k1 = KeysightState(name="K01", keysight_ip="169.254.5.2", timeout_ms=5000)
+k2 = KeysightState(name="K02", keysight_ip="169.254.124.255", timeout_ms=5000)
+k1.get_device_info()
+k2.get_device_info()
+
+k1.on()
+k2.on()
+
+k1.acquire()
+k2.acquire()
+d = k1.read_data(wait=True)
+print(d)
+d = k2.read_data(wait=True)
+
+k1.close()
+k2.close()
