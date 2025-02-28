59bd9c238fc20f8adc4662aff54cf8d686dcfd49
estevesjh <johnny.udi@gmail.com>
2025-02-27 21:50:11 -0300

    finished transmission scripts to be tested

--- pandora/commands/measure_nd_transmission.py
+++ pandora/commands/measure_nd_transmission.py
@@ -5,24 +5,51 @@ Command line interface for measuring ND filter transmission.
 Usage:
     pb measure-nd-transmission --lambda0 400 --lambdaEnd 700 --filters ND1,ND2
 
+    Steps:
+    1. Move to a specific ND filter
+    2. Measure the transmission of the ND filter (wavelength scan)
+    3. Save the transmission data to a file
+    4. Repeat for all specified ND filters
+
 """
+import numpy as np
+from pandora.calibration.transmission import measureTransmission
+from pandora.calibration.transmission import measureNDFactor
+
 def measureNDTransmission(args):
     from pandora.pandora_controller import PandoraBox   
-    pandora_box = PandoraBox(config_file="../default.yaml", verbose=True)
+    pandora_box = PandoraBox(config_file="../../default.yaml", verbose=True)
+
+    # Take the measurements with no ND filter
+    pandora_box.move_nd_filter("CLEAR")
+    pandora_box.turn_on_sollar_cell()
+
+    # Select the optical mask
+    # TODO: Implement move_optical_mask
+    pandora_box.move_optical_mask(args.maskPorts)
 
     for i, ND in enumerate(args.filters.split(",")):
         pandora_box.move_nd_filter(ND)
-        pandora_box.wavelegth_scan(args.lambda0, args.lambdaEnd, args.step)
+        # Scan the wavelength in linear steps from lambda0 to lambdaEnd
+        # Takes one dark, one exposure, one dark, moves to the next wavelength
+        pandora_box.wavelegth_scan(args.lambda0, args.lambdaEnd, args.step, 
+                                   observation_type="throughput", nrepeats=args.nrepeats)
+
+    # Compute the transmission for each ND filter
+    lambdaBins = np.arange(args.lambda0-args.step/2, args.lambdaEnd+args.step*3/2, args.step)
     
-    saveNDTransmissionCalibrationFile(pandora_box)
-    pass
-
-## TODO: Make a calibration folder
-## TODO: Make a standard calibration file name
-def saveNDTransmissionCalibrationFile(pandora_box):
-    """
-    Save the ND transmission calibration file.
-
-    """
+    # Load the latest throughput calibration if fname is None
+    th = pandora_box.load_pandora_throughput(args.fname)
+    
+    # Get the ND filter data
     df = pandora_box.get_database()
-    df.to_csv("NDTransmissionCalibration.csv")
+    # Get the QE curve
+    qeCurve = pandora_box.get_qe_solarcell()
+
+    # Compute the transmission for each ND filter
+    for i, ND in enumerate(args.filters.split(",")):
+        df_nd = df[df['ND'] == ND].copy()
+        transmission = measureTransmission(df_nd, qeCurve, specBins=lambdaBins)
+        nd_ratio = measureNDFactor(transmission, th)
+        pandora_box.write_calibration(nd_ratio, f"transmission_{ND}")
+    pass
--- pandora/commands/measure_pandora_throughput.py
+++ pandora/commands/measure_pandora_throughput.py
@@ -3,26 +3,51 @@
 Command line interface for measuring pandora throughput.
 
 Usage:
-    pb measure-pandora-throughput --lambda0 300 --lambdaEnd 700 --step 1 --maskPorts
+    pb measure-pandora-throughput --lambda0 300 --lambdaEnd 700 --step 1 --maskPorts A
 
 """
+
+from pandora.calibration.transmission import measureTransmission
+import numpy as np
+
 def measurePandoraThroughput(args):
+    print("Measuring Pandora throughput")
+
     from pandora.pandora_controller import PandoraBox   
     pandora_box = PandoraBox(config_file="../default.yaml", verbose=True)
 
+    print("Clear the optical path, no ND filters")
+    # Take the measurements with no ND filter
     pandora_box.move_nd_filter("CLEAR")
     pandora_box.turn_on_sollar_cell()
-    pandora_box.wavelegth_scan(args.lambda0, args.lambdaEnd, args.step, observation_type="throughput")
-    saveNDTransmissionCalibrationFile(pandora_box)
 
-    pass
+    # Select the optical mask
+    # TODO: Implement move_optical_mask
+    print("Select the optical mask")
+    pandora_box.move_optical_mask(args.maskPorts)
 
-## TODO: Make a calibration folder
-## TODO: Make a standard calibration file name
-def saveNDTransmissionCalibrationFile(pandora_box):
-    """
-    Save the ND transmission calibration file.
+    print(f"Scanning the wavelength from {args.lambda0} to {args.lambdaEnd} in steps of {args.step} with {args.nrepeats} repeats")
+    print("this might take a while...")
+    # Scan the wavelength in linear steps from lambda0 to lambdaEnd
+    # Takes one dark, one exposure, one dark, moves to the next wavelength
+    pandora_box.wavelegth_scan(args.lambda0, args.lambdaEnd, args.step, observation_type="throughput", nrepeats=args.nrepeats)
 
-    """
+    # Compute the throughput
+    lambdaBins = np.arange(args.lambda0-args.step/2, args.lambdaEnd+args.step*3/2, args.step)
+    
+    # get solar cell qe curve
+    print("Getting solar cell QE curve")
+    qeCurve = pandora_box.get_qe_solarcell()
+
+    # get the data taken
     df = pandora_box.get_database()
-    df.to_csv("NDTransmissionCalibration.csv")
+
+    print("Calculating throughput")
+    transmission = measureTransmission(df, qeCurve, specBins=lambdaBins)
+    
+    print("Saving throughput calibration file")
+    # Save the throughput calibration file
+    pandora_box.write_calibration(transmission, "throughput")
+
+    print("Done! Thanks for waiting")
+    pass
