0de8eb518b8af2e40b1452d4597c81debd8c9ab5
estevesjh <johnny.udi@gmail.com>
2025-03-19 17:43:35 -0300

    good lines

--- pandora/calibration/hg2_lamp.py
+++ pandora/calibration/hg2_lamp.py
@@ -14,16 +14,54 @@ import numpy as np
 hg2_lines = {
     # Mercury (Hg) lines
     "Hg01": 253.65200,
-    "Hg02": 296.72840,
-    "Hg03": 302.15060,
-    "Hg04": 313.15560,
+    # "Hg02": 296.72840,
+    # "Hg03": 302.15060,
+    "Hg04": 313.155,  # blended line
     "Hg05": 334.14820,
-    "Hg06": 365.01520,
+    "Hg06": 365.0152,  # blended line
     "Hg07": 404.65650,
     "Hg08": 435.83430,
     "Hg09": 546.07350,
     "Hg10": 576.95980,
-    "Hg11": 579.06630,
+
+    # Argon (Ar) lines
+    # "Ar01":  696.54310,
+    # "Ar02":  706.72180,
+    # "Ar03":  714.70420,
+    "Ar04":  727.29360,
+    "Ar05":  738.39800,
+    "Ar06":  750.3869,  # blended line
+    "Ar07":  763.51100,
+    "Ar08":  772.376,   # blended line
+    "Ar09":  794.81760,
+    "Ar10":  800.616,   # blended line
+    "Ar11":  811.531,   # blended line
+    "Ar12":  826.45180,
+    "Ar13":  842.465,   # blended line
+    "Ar14":  852.14400,
+    "Ar15":  866.79400,
+    "Ar16":  912.29670,
+    "Ar17":  922.44990,
+    "Ar18":  935.42200,
+    "Ar19":  965.77860,
+    "Ar20":  978.45100,
+    "Ar21":  1047.0050,
+    # "Ar22":  1067.3570,
+    # "Ar23":  1107.8870,
+}
+hg2_lines_updated = {
+    # Mercury (Hg) lines
+    "Hg01": 253.65200,
+    "Hg02": 296.72840,
+    "Hg03": 302.15060,
+    "Hg04": 312.960,  # updated line peak (two lines are close)
+    "Hg05": 334.14820,
+    "Hg06": 365.235,  # updated line peak (two lines are close)
+    "Hg07": 404.65650,
+    "Hg08": 435.83430,
+    "Hg09": 546.07350,
+    "Hg10": 576.95980,
+    # "Hg11": 579.06630,
 
     # Argon (Ar) lines
     "Ar01":  696.54310,
@@ -31,24 +69,24 @@ hg2_lines = {
     "Ar03":  714.70420,
     "Ar04":  727.29360,
     "Ar05":  738.39800,
-    "Ar06":  750.38690,
+    "Ar06":  750.819,  # updtaded line peak (two lines are close)
     "Ar07":  763.51100,
-    "Ar08":  772.37610,
+    "Ar08":  772.394,  # updated line peak (two lines are close)
     "Ar09":  794.81760,
-    "Ar10":  800.61580,
-    "Ar11":  811.53110,
+    "Ar10":  801.106,  # updated line peak (two lines are close)
+    "Ar11":  811.176,  # updated line peak (two lines are close)
     "Ar12":  826.45180,
-    "Ar13":  842.46480,
-    "Ar14":  912.29670,
-    "Ar15":  922.44990,
-    "Ar16":  935.42200,
-    "Ar17":  949.74290,
-    "Ar18":  965.77860,
-    "Ar19":  978.45100,
-    "Ar20": 1047.00000,
-    "Ar21": 1066.66000,
-    "Ar22": 1088.46000,
-    "Ar23": 1090.65000,
+    "Ar13":  841.956,  # updated line peak (two lines are close)
+    "Ar14":  852.14400,
+    "Ar15":  866.79400,
+    "Ar16":  912.29670,
+    "Ar17":  922.44990,
+    "Ar18":  935.42200,
+    "Ar19":  965.77860,
+    "Ar20":  978.45100,
+    "Ar21":  1047.0050,
+    "Ar22":  1067.3570,
+    "Ar23":  1107.8870,
 }
 
 # Approximate relative intensities (peak heights). Actual values
@@ -66,7 +104,7 @@ hg2_line_strengths = {
     "Hg08": 4000,
     "Hg09": 3200,
     "Hg10": 1200,
-    "Hg11": 1000,
+    # "Hg11": 1000,
 
     # Argon lines
     "Ar01":  600,
@@ -135,3 +173,9 @@ if __name__ == "__main__":
     plt.xlabel("Wavelength (nm)")
     plt.ylabel("Intensity (arbitrary units)")
     plt.title('Fake Hg2 Lamp Spectrum')
+
+    from astroquery.nist import Nist
+    import astropy.units as u
+    hg2 = Nist.query(4000 * u.AA, 6000 * u.AA, linename="Hg I")
+    ar = Nist.query(4000 * u.AA, 6000 * u.AA, linename="Ar I")
+    
