09c7e09d784ef1f6a7c2b9f413ff067b2c27f733
estevesjh <johnny.udi@gmail.com>
2025-03-22 10:04:49 -0300

    handling labjack disconnection

--- pandora/states/labjack_handler.py
+++ pandora/states/labjack_handler.py
@@ -32,14 +32,19 @@ class LabJack:
         self.handle = None
         self.logger = logging.getLogger("LabJack")
         self.verbose = verbose
+        self.wait_time_ms = 1000
+
+        self.state = None
         self.initialize()
 
     def initialize(self):
         try:
             # Open a connection to the LabJack device at the given IP address
             self.handle = ljm.openS("ANY", self.tag, self.address)
+            self.state = "on"
             self.logger.info(f"Connected to LabJack at {self.address}")
         except Exception as e:
+            self.state = "off"
             self.logger.error(f"Failed to connect to LabJack: {e}")
             raise
 
@@ -57,7 +62,13 @@ class LabJack:
             self.logger.info(f"Read value {value} from register {port_name}")
             return value
         except Exception as e:
-            self.logger.error(f"Failed to read from LabJack: {e}")
+            self.state = "off"
+            self.reset()
+            if self.state=="on":
+                self.read(port_name)
+            else:
+                self.logger.error(f"Failed to read from LabJack: {e}")
+
             raise
 
     def close(self):
@@ -72,9 +83,14 @@ class LabJack:
             self.logger.info("Sending high signal to the system...")
             self.write(port_name, 1)
             self.logger.info(f"Signal high sent to {port_name}.")
+        # try one reset if it fails
         except Exception as e:
             self.logger.error(f"Error send_high_signal Monochromator: {e}")
-            self.set_error()
+            self.state = "off"
+            self.reset()
+            if self.state == "on":
+                self.send_high_signal(port_name)
+
 
     def send_low_signal(self, port_name):
         # self.logger.info("Sending low signal to labjack entry.")
@@ -85,7 +101,10 @@ class LabJack:
             self.logger.info(f"Signal low sent to {port_name}.")
         except Exception as e:
             self.logger.error(f"Error send_low_signal Monochromator: {e}")
-            self.set_error()
+            self.state = "off"
+            self.reset()
+            if self.state == "on":
+                self.send_low_signal(port_name)
 
     def send_binary_signal(self, port_name, wait_time_ms=50):
         self.logger.info("Sending square signal to labjack entry.")
@@ -105,7 +124,24 @@ class LabJack:
             if self.verbose:
                 print(f"Failed to get device info: {e}")
             raise
+    
+    def set_error(self):
+        # Set error state
+        self.logger.error("Error occurred.")
+        pass
 
+    def reset(self):
+        time.sleep(self.wait_time_ms / 1000)
+        try:
+            # Reset the device
+            self.logger.info("Resetting the device...")
+            self.initialize()
+            self.logger.info("Device reset.")
+        except Exception as e:
+            self.state = "off"
+            self.logger.error(f"Failed to reset device: {e}")
+            raise
+        
 if __name__ == "__main__":
     # Usage example
     ip_address = str(input("Enter the IP address of the LabJack device: "))
